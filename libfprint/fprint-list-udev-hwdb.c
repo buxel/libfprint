9992fc126aa20e869aed27b3efdf485f4c268e18
Dev <dev@local>
2026-02-19 16:06:40 +0000

    hwdb/style: add GF511 hwdb entry and clang-format config
    fprint-list-udev-hwdb.c: remove 27c6:5110 from the known-unsupported
    devices table now that it is handled by the goodixtls511 driver.
    Leaving it there caused fprint-list-udev-hwdb to warn under
    G_DEBUG=fatal-warnings, crashing the udev-hwdb CI test.
    
    .clang-format: add GNU-based style config (2-space indent, 90-col
    limit) and apply it to all new goodixtls driver and sigfm files.
    Core libfprint files are formatted separately to keep their diff
    minimal.
    
    Regenerate autosuspend.hwdb: add 27c6:5110 to supported devices

--- libfprint/fprint-list-udev-hwdb.c
+++ libfprint/fprint-list-udev-hwdb.c
@@ -126,7 +126,7 @@ static const FpIdEntry allowlist_id_table[] = {
   { .vid = 0x2541, .pid = 0x0236 },
   { .vid = 0x2541, .pid = 0x9711 },
   { .vid = 0x27c6, .pid = 0x5042 },
-  { .vid = 0x27c6, .pid = 0x5110 },
+  /* 0x5110 removed: now supported by goodixtls511 driver */
   { .vid = 0x27c6, .pid = 0x5117 },
   { .vid = 0x27c6, .pid = 0x5120 },
   { .vid = 0x27c6, .pid = 0x5125 },
